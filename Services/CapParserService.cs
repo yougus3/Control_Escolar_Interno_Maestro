4dfae84f48634028b14749f241df951d9de76cb7
Gustavo <gustavodevmiranda@gmail.com>
2026-05-26 01:52:10 -0600

    CORRECIÓN GIT

---
+++ Services/CapParserService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Registro_de_Calificaciones_Jose_Ma._Morelos_y_Pavon.Services
+{
+    internal class CapParserService
+    {
+    }
+}
---
+++ Views/Modals/ExportImportView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Registro_de_Calificaciones_Jose_Ma._Morelos_y_Pavon.Views.Modals
+{
+    /// <summary>
+    /// Lógica de interacción para ExportImportView.xaml
+    /// </summary>
+    public partial class ExportImportView : UserControl
+    {
+        public ExportImportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
