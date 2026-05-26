6a193a4c63e35eb5206b699cb21ae13ef8c5995a
Gustavo <gustavodevmiranda@gmail.com>
2026-05-26 01:46:36 -0600

    Estructura de desarrollo

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Registro_de_Calificaciones_Jose_Ma._Morelos_y_Pavon;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
